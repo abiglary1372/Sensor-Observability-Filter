aab182f1c31818e66542ce5681865b79f5b3d08b
Amin Biglary Makvand <abiglary@gmail.com>
2022-02-25 17:57:36 +0100

    update

--- test2.py
+++ test2.py
@@ -1,16 +1,47 @@
 #test2 
 import numpy as np
 
+G=[[1,0,0,0,0,0,0,0,0,0],
+    [0,1,0,0,0,0,0,0,0,0],
+    [0,0,1,0,0,0,0,0,0,0],
+    [0,0,-0.5547,0.8321,0,0,0,0,0,0],
+    [0.3201,0.5976,0.3320,0.4980,0.4268,0,0,0,0,0],
+    [0,0,0.8602,0.3982,0,0.3186,0,0,0,0],
+    [0.3243,0.5202,0.1788,0.5364,0.5515,0,0.0215,0,0,0],
+    [0.1834,0,0.8359,0.3203,0,0.3669,0,0.1747,0,0],
+    [0.2100,0.0321,0.8309,0.3231,0.0634,0.3497,0,0.1832,0.0083,0],
+    [0.2318,0.0608,0.8217,0.3259,0.1245,0.3310,0.0015,0.1891,0.0164,0.0007]]
+a1 = [0,0,0,0,0,0,0,0,2,0]
 
-def max_beta(Br):
-    maxBta = [Br[0][0],Br[0][1]]
-    i=0
-    while i<len(Br):
-        if len(maxBta[0]) < len(Br[i][0]):
-            maxBta = Br[i]
-        i=i+1
-    return maxBta
+InvBs = np.linalg.inv(np.array(G))
 
-Br = [[[], [3, 1, 0, 0, 0]], [[1,2,3,4], [6, 0, 0, 0, 0]], [[], [6, 0, 7, 0, 0]], [[], [6, 3, 0, 0, 0]]]
+Sv1 = np.dot(np.array(a1),np.array(G))
 
-H = max_beta(Br)
+a1nw = np.dot(Sv1,InvBs)
+
+i=0
+while i<len(a1nw):
+    #compensating numerical errors
+    if abs(a1nw[i]) < 1e-10:
+        a1nw[i]=0
+    i=i+1
+    
+A=[[0,0,0,0,0,0,0,0,2,0],[0,1,0,0,0,3,0,0,2,0],[0,0,0,4,0,0,0,0,2,0]]
+
+def snsr_fndr(G,Alpha):
+    
+    #this function is used to design a sensor set for our numerical example
+    Sv = []
+    l=0
+    while l < len(Alpha):
+        s =np.dot(np.array(Alpha[l]),np.array(G))
+        s.tolist()
+        
+        Sv.append(s)
+        l=l+1
+        
+    return Sv
+
+
+Set = snsr_fndr(G,A)
+
